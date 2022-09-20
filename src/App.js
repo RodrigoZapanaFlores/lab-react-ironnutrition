ae459dda85d676f174b227368ef3b4574a87d406
RodrigoZapanaFlores <rodrigozapanaflores@gmail.com>
2022-09-20 13:08:45 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { Divider, Row, Button } from 'antd';
+import FoodBox from './components/FoodBox';
 import './App.css';
+import AddFoodForm from './components/AddFoodForm';
+import foods from './foods.json';
+import { useState } from 'react';
+import SearchBar from './components/SearchBar';
 
 function App() {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [calories, setCalories] = useState(0);
+  const [servings, setServings] = useState(1);
+  const [foodArray, setFoodArray] = useState(foods);
+  const [search, setSearch] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    foodArray.push({ name, calories, image, servings });
+    setFoodArray([...foodArray]);
+  };
+
+  const handleDelete = (name) => {
+    setFoodArray(foodArray.filter((f) => name !== f.name));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <AddFoodForm
+        handleSubmit={handleSubmit}
+        name={name}
+        setName={setName}
+        image={image}
+        setImage={setImage}
+        calories={calories}
+        setCalories={setCalories}
+        servings={servings}
+        setServings={setServings}
+      />
+
+      <Button>Search / Hide</Button>
+
+      <Divider> Search </Divider>
+
+      <SearchBar search={search} setSearch={setSearch} />
+
+      <Divider> Food List </Divider>
+
+      {foodArray.length === 0 ? (
+        <div className='no-more-items'> 
+          <h3>Oops! Thre is no more content to show.</h3>
+          <img src="https://cdn-icons-png.flaticon.com/512/594/594468.png" alt="no more items" />
+        </div>
+      ) : (
+        <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {foodArray
+          .filter((food) =>
+            food.name.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((food) => (
+            <FoodBox key={food.name} {...food} handleDelete={handleDelete} />
+          ))}
+      </Row>
+      )}      
     </div>
   );
 }
